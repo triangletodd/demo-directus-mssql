d2a3a63928bfcd4df5cce9ff6e12fab9f76b33e2
Todd Edwards <todd@mtn.cc>
2022-10-14 10:31:46 -0400

    Share latest changes

--- mssql/configure-db.sh
+++ mssql/configure-db.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
 
+set -e
+set -x
+
 # Wait 60 seconds for SQL Server to start up by ensuring that
 # calling SQLCMD does not return an error code, which will ensure that sqlcmd is accessible
 # and that system and user databases return "0" which means all databases are in an "online" state
 # https://docs.microsoft.com/en-us/sql/relational-databases/system-catalog-views/sys-databases-transact-sql?view=sql-server-2017
 
-DBSTATUS=1
-ERRCODE=1
-i=0
-
 sqlcmdq() {
   /opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -d master -Q "$@"
 }
+
+DBSTATUS=1
+ERRCODE=1
+i=0
 
 while [[ $DBSTATUS -ne 0 ]] && [[ $i -lt 60 ]] && [[ $ERRCODE -ne 0 ]]; do
 	i=$i+1
